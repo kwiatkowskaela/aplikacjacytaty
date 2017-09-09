7d2801e2265f754b2991ed89b861c4b8876f0b7c
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-09-09 18:02:08 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -18,8 +18,7 @@ function createTweet(input) {
     var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
    
     if (tweetText.length > 140) {
-        getQuote();
-    } else {
+        
         var tweet = tweetLink + encodeURIComponent(tweetText);
         $('.quote').text(quoteText);
         $('.author').text("Author: " + quoteAuthor);
