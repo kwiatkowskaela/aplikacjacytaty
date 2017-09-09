6a87ef11e14ccbec8b8cc47e919ade2d9bab8888
kwiatkowskaela <kwiatkowska.grafik@gmail.com>
2017-09-09 15:45:56 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
 function getQuote() {
     $.getJSON(quoteUrl, createTweet);
 }
+
 function createTweet(input) {
     var data = input[0];
 
@@ -12,5 +14,21 @@ function createTweet(input) {
     if (!quoteAuthor.length) {
         quoteAuthor = "Unknown author";
     }
-  var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;Copy
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    Copy
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+    $('.tweet').attr('href', tweet);
+    $(document).ready(function () {
+        getQuote();
+        $('.trigger').click(function () {
+            getQuote();
+        })
+    });
 }
